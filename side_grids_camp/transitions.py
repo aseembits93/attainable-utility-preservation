61e5eef7cb6433339fffc208df12a1bee63a4308
Gavin Leech <gleech@protonmail.com>
2018-04-19 17:03:16 +0100

    Update working but incompatible version

--- side_grids_camp/transitions.py
+++ side_grids_camp/transitions.py
@@ -1,9 +1,13 @@
 from ai_safety_gridworlds.environments.shared.safety_game import Actions
 from ai_safety_gridworlds.environments.shared.rl.environment import TimeStep
+
 from hashlib import sha1
+import numpy
+import time
+from IPython import display
+import copy
 
 ACTIONS = [ a for a in Actions if a is not Actions.QUIT ]
-
 
   
 def merge_two_dicts(x, y):
@@ -11,30 +15,75 @@ def merge_two_dicts(x, y):
     z.update(y)
     return z
 
+
+def hash_board(state, stateType='RGB') :
+    if type(state) == TimeStep :
+        state = state.observation[stateType]
+    return sha1(state).hexdigest()
+
+
+def to_grayscale(state, sess) :
+    if type(state) == TimeStep :
+        state = state.observation['RGB']
+
+    state = np.moveaxis(state, 0, -1)
+    resize = tf.placeholder(shape=list(state.shape), dtype=tf.uint8)
+    gray_frame = tf.squeeze( tf.image.rgb_to_grayscale(state) )
+    grayed = sess.run( gray_frame, { resize: state } )
   
-def hash_board(state) :
-    return sha1(state.observation['RGB']).hexdigest()  
+    return grayed
 
 
-def build_env_from_bitmap(boardState, gridSize=6) :
-    strings = ["%.0f" % x for x in boardState.reshape(boardState.size) ]
-    rows = [strings[i:i+gridSize] for i in range(0, len(strings), gridSize) ]
-    rows = [[x.replace('0','#') for x in l] for l in rows]
-    rows = [[x.replace('1',' ') for x in l] for l in rows]
-    rows = [[x.replace('2','A') for x in l] for l in rows]
-    rows = [[x.replace('3','C') for x in l] for l in rows]
-    rows = [[x.replace('4','X') for x in l] for l in rows]
-    rows = [[x.replace('5','G') for x in l] for l in rows]
-    
-    return sokoban_game(rows)
 
-
-def recurse_for_states(envi, state, hashes) :
+  
+def recurse_for_substates(envi, state, hashes) :
     subhashes = crawl_for_states(envi, state, hashes)
     hashes = merge_two_dicts(subhashes, hashes)
     
     return hashes
 
+
+def build_env_from_boardmap(boardState, gridSize=6) :
+    boardToArtMap = {
+        '0': '#',
+        '1': ' ',
+        '2': 'A',
+        '3': 'C',
+        '4': 'X',
+        '5': 'G'
+    }
+    return build_env_from_bitmap(boardState, boardToArtMap, gridSize)
+
+  
+def build_env_from_grayscale(grayState, gridSize=6) :
+    grayscaleToArtMap = {
+        '152': '#',
+        '219': ' ',
+        '134': 'A',
+        '3'  : 'C',
+        '78' : 'X',
+        '129': 'G'
+    }
+    return build_env_from_bitmap(grayState, grayscaleToArtMap, gridSize)
+
+
+# `valueMap` is a dict of object codes to game art codes
+def build_env_from_bitmap(state, valueMap, gridSize=6) :
+    rows = get_game_rows_from_state(state, gridSize)
+    
+    for k,v in valueMap.items() :
+      rows = [ [x.replace(k,v) for x in l] for l in rows ]
+      
+    return sokoban_game(level=0, game_art=[rows])
+  
+  
+# Returns list of list of strings from the np state passed in
+def get_game_rows_from_state(state, gridSize):
+    strings = ["%.0f" % x for x in state.reshape(state.size) ]
+    chunkedIntoRows = range(0, len(strings), gridSize)
+    
+    return [ strings[i:i+gridSize] for i in chunkedIntoRows ]
+  
 
 """
   # Crawl state space to get all occupyable states (agent-box pairs).
@@ -42,32 +91,33 @@ def recurse_for_states(envi, state, hashes) :
   * `transitions`, a dict from (state, action, nextState) to 1 or 0.
   * `hashMap`, a dict from state-hash to ndarray state
 """
-def crawl_for_states(envir, lastState, hashMap) :  
+def crawl_for_states(envir, lastState, hashMap, stateType='board') :  
   for action in ACTIONS :
       frozenEnv = copy.deepcopy(envir)
       nextState = frozenEnv.step(action)
-      lastIndex = hash_board(lastState)
-      index = hash_board(nextState)
+      lastIndex = hash_board(lastState, stateType)
+      index = hash_board(nextState, stateType)
       
       # Stop infinite regress by checking history:
       if index not in hashMap :
-          hashMap[index] = nextState.observation['board']
+          hashMap[index] = nextState.observation[stateType]
           
+          # If `action` changed the state, recurse:
           if not lastIndex == index :
               lastState = nextState
-              hashMap = recurse_for_states(frozenEnv, lastState, hashMap)
+              hashMap = recurse_for_substates(frozenEnv, lastState, hashMap)
   
   return hashMap 
 
 
+
 # Returns a set of (state, action, state) tuples for all actions at all occupyable states.
-# Needs to be supplemented with zero probabilities: 
-# if a (state, action, state) not in this matrix, it's zero.
+# Needs to be supplemented with zero probabilities.
 def crawl_for_transitions(allStates):    
     transitions = {}
     
     for state in allStates.values() :
-        envir = build_env_from_bitmap(state)
+        envir = build_env_from_boardmap(state)
         lastState = envir.reset()
         lastIndex = hash_board(lastState)
         
@@ -79,22 +129,17 @@ def crawl_for_transitions(allStates):
     return transitions
 
 
-
 def get_transitions(envi, initialState, states={}):
     states = crawl_for_states(envi, initialState, states)
-    possibleTransitions = crawl_for_transitions(states)
     
-    # Then rename all the hashes as integer indices?:
-    #for i,k in enumerate(states.keys()) :
-    #  states[i] = states.pop(k)
-    
-    return possibleTransitions
+    return crawl_for_transitions(states)
 
 
 def test_transition_crawler() :
-    GAME_ART = ['######', '# A###', '# X  #', '##   #', '### G#', '######']
-    envi = sokoban_game(GAME_ART)
+    GAME_ART = [['######', '# A###', '# X  #', '##   #', '### G#', '######']]
+    envi = sokoban_game(level=0, game_art=GAME_ART)
     initialState = envi.reset()
+    #im = plt.imshow(get_frame(initialState, 0, -1), animated=True)
     hashes = {}
     hashMap = crawl_for_states(envi, initialState, hashes)
     
@@ -103,26 +148,6 @@ def test_transition_crawler() :
     
     transitions = get_transitions(envi, initialState, hashMap)
     assert( len(transitions) == 240 )
-    
-    pushState = envi.step(Actions.DOWN)
-    pushIndex = hash_board(pushState)
-    startDown = (initialIndex, Actions.DOWN, pushIndex)
-    assert( transitions[(initialIndex, Actions.DOWN, pushIndex )] == 1)
-    
-    envi.reset()
-    cornerState = envi.step(Actions.LEFT)
-    cornerIndex = hash_board(cornerState)
-    startToLeft = (initialIndex, Actions.LEFT, cornerIndex)
-    assert( transitions[startToLeft] == 1)
 
 
-
-"""
-# Usage:
-GAME_ART = ['######', '# A###', '# X  #', '##   #', '### G#', '######']
-env = sokoban_game(GAME_ART)
-step = env.reset()
-get_transitions(env, step, states={})
-
-test_transition_crawler()
-"""
+#test_transition_crawler()
